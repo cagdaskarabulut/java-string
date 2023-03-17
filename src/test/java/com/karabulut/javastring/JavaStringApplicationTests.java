f285b4028c21724382ab217b0ac06fadc382997d
cagdas_karabulut <ckarabulut@adersystems.com>
2023-03-17 12:01:29 +0300

    .

---
+++ src/test/java/com/karabulut/javastring/JavaStringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karabulut.javastring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
